ef97945ca6c48234013aa5f48f914d6b2a633fff
bhushanvasisht <Herambha725>
2020-12-10 10:51:36 -0600

    bootstrap setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <p>Welcome to ProShop</p>
+        <Header />
+        <main className={'py-3'}><Container><h1>Welcome to ProShop</h1></Container></main>
+        <Footer />
     </>
   )
 }
